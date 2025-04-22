4bc777650bcd8bd806a7e05dcf11136326cd22c3
Romário de Melo Damasceno <romario.damasceno@atlasinovacoes.com.br>
2025-04-22 03:25:02 -0300

    Implementado o serviços de extensão para a aplication

---
+++ src/OrderCalc.Application/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using OrderCalc.Application.Interfaces;
+using OrderCalc.Application.Service;
+using OrderCalc.Domain.Interfaces.Services;
+using OrderCalc.Domain.Services;
+
+
+namespace OrderCalc.Application.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services)
+    {
+        services.AddScoped<IOrderServiceAplication, OrderServiceAplication>();
+        services.AddScoped<IOrderService, OrderService>();    
+    }
+}
