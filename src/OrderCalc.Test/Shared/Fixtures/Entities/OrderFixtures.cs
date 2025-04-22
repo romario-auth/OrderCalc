0e51199d04bc5a1aec86243df8648706a1cffd45
Romário de Melo Damasceno <romario.damasceno@atlasinovacoes.com.br>
2025-04-22 03:16:21 -0300

    Ajustando as entidades e as classes de testes para os Ids serem definidos pelo usuários

--- src/OrderCalc.Test/Shared/Fixtures/Entities/OrderFixtures.cs
+++ src/OrderCalc.Test/Shared/Fixtures/Entities/OrderFixtures.cs
@@ -5,11 +5,12 @@ namespace OrderCalc.Test.Shared.Fixtures.Entities;
 public class OrderFixtures
 {
     private int _customerId = 1;
+    private int _id = 1;
 
     public static OrderFixtures New() => new OrderFixtures();
 
     public Order Builder()
     {
-        return Order.Create(_customerId);
+        return Order.Create(_id, _customerId);
     }
 }
